f4afc57680b931447266a76f053634fe7ff37a6a
sensualscientist <31266468+sensualscientist@users.noreply.github.com>
2017-08-28 12:01:34 -0700

    Added tagline under site name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 export default () => {
 	return(
 		<header>
-			<div><Link to='/' className='logo'>Let's Experiment!</Link></div>
+			<div><Link to='/' className='logo'>Let's Experiment!</Link></div><br />
+			<div>Experiment with your partner!</div>
 		</header>
 	);
 }
